889a0b5d59be03112828523ac4074714638108f8
Joshue Barrios <joshuedavidbarrios@gmail.com>
2022-02-26 01:15:52 -0300

    first commit

---
+++ js/kaliman.js
@@ -0,0 +1,7 @@
+//debugger
+let entrada = prompt('Ingresa tú nombre: ');
+let mensaje1 = " Hola "
+let mensaje = " Bienvenido al inicio de mí proyecto de JS,  DISFRUTAA!! ";
+let resultado = mensaje1  +  ''  +  entrada  +  ''  +  mensaje;
+
+alert(resultado);
